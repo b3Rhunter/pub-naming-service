27b3ba13df093b2fefba7c32aa9b0487283aa178
Pub <96320073+b3Rhunter@users.noreply.github.com>
2023-02-27 06:39:27 -0600

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,113 @@
+import React, { useState, useEffect } from "react";
+import { ethers } from "ethers";
+import PNS_ABI from "./PNS.json";
+import './App.css';
+
+const PNS_ADDRESS = "0x0d5c70a93Cce712473AEe6BcBfb7CDbb5daa8D9b";
+
+function App() {
+
+  const [domainName, setDomainName] = useState("");
+  const [primaryName, setPrimary] = useState("");
+  const [connected, setConnected] = useState(false);
+  const [pubName, setPubName] = useState("");
+
+
+  const connect = async () => {
+    try {
+      const provider = new ethers.providers.Web3Provider(window.ethereum);
+      await provider.send("eth_requestAccounts", []);
+      setConnected(true)
+      const signer = await provider.getSigner();
+      const address = await signer.getAddress()
+      const pnsContract = new ethers.Contract(PNS_ADDRESS, PNS_ABI, signer);
+      const name = await pnsContract.getPrimaryDomain(address);
+      console.log(name)
+      setPubName(name)
+    } catch (error) {
+      console.log(error.message)
+    }}
+
+  async function registerDomain(domainName) {
+    try {
+      const provider = new ethers.providers.Web3Provider(window.ethereum);
+      await provider.send("eth_requestAccounts", []);
+      const signer = provider.getSigner();
+      const pnsContract = new ethers.Contract(PNS_ADDRESS, PNS_ABI, signer);
+      const transaction = await pnsContract.registerDomain(domainName);
+      await transaction.wait();
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  const handleRegisterDomain = async (event) => {
+    event.preventDefault();
+    await registerDomain(domainName);
+  };
+
+
+  async function setPrimaryDomain(primaryName) {
+    try {
+      const provider = new ethers.providers.Web3Provider(window.ethereum);
+      await provider.send("eth_requestAccounts", []);
+      const signer = provider.getSigner();
+      const pnsContract = new ethers.Contract(PNS_ADDRESS, PNS_ABI, signer);
+      const transaction = await pnsContract.setPrimaryDomain(primaryName);
+      await transaction.wait();
+      setPubName(transaction)
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  async function handleSetPrimaryDomain(event) {
+    event.preventDefault();
+    await setPrimaryDomain(primaryName);
+    setPubName(primaryName)
+  }
+
+  return (
+    <div className="App">
+      <header className="App-header">
+
+      <button onClick={connect}>connect</button>
+      {connected && (
+        <p>welcome back...{pubName}</p>
+      )}
+      {!connected && (
+        <p>register and set a primary .pub domain</p>
+      )}
+
+      <form onSubmit={handleRegisterDomain}>
+        <input
+          type="text"
+          placeholder="domain name"
+          value={domainName}
+          onChange={(event) => setDomainName(event.target.value)}
+        />
+        &nbsp;
+        <button type="submit">Register domain</button>
+      </form>
+
+
+      <div>
+        <form onSubmit={handleSetPrimaryDomain}>
+          <input
+            type="text"
+            placeholder="me.pub"
+            value={primaryName}
+            onChange={(event) => setPrimary(event.target.value)}
+          />
+        &nbsp;
+        <button type="submit">Set primary domain</button>
+        </form>
+      </div>
+
+
+      </header>
+    </div>
+  );
+}
+
+export default App;
